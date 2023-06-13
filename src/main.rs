4934c0665f52015a907d82da0cc924d7c7b60370
navigaid <navigaid@gmail.com>
2023-06-13 05:33:29 +0000

    init async main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use libp2p::{identity, PeerId};
+use std::error::Error;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>>{
     println!("Hello, world!");
+    Ok(())
 }
