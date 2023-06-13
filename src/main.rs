1af0d252bab5604877529db7bb1451724a16b458
navigaid <navigaid@gmail.com>
2023-06-13 05:52:59 +0000

    add network behaviour
    https://docs.rs/libp2p/0.51.3/libp2p/tutorials/ping/index.html#network-behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use libp2p::{identity::Keypair, PeerId};
+use libp2p::swarm::{keep_alive, NetworkBehaviour};
+use libp2p::{identity::Keypair, ping, PeerId};
 use std::error::Error;
 
 const ZERO_KEY: [u8; 32] = [0u8; 32];
@@ -10,5 +11,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let local_peer_id = PeerId::from(local_key_pair.public());
     println!("Local peer id: {local_peer_id:?}!");
     let transport = libp2p::development_transport(local_key_pair).await?;
+    let behaviour = Behaviour::default();
     Ok(())
+}
+
+/// Our network behaviour.
+///
+/// For illustrative purposes, this includes the [`KeepAlive`](behaviour::KeepAlive) behaviour so a continuous sequence of
+/// pings can be observed.
+#[derive(NetworkBehaviour, Default)]
+struct Behaviour {
+    keep_alive: keep_alive::Behaviour,
+    ping: ping::Behaviour,
 }
